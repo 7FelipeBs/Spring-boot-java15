61188e1793638112131a2782389969f04911a0fd
Felipe Bs <programming1129@gmail.com>
2021-02-17 19:28:03 -0300

    Iniciando Projeto

---
+++ src/main/java/com/publicenter/reactcity/ReactCityApplication.java
@@ -0,0 +1,13 @@
+package com.publicenter.reactcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactCityApplication.class, args);
+	}
+
+}
